e9e4dd840fa0f8cf02727c4ce216f23ac869f9f5
KensanL <gzhongq@163.com>
2014-10-26 13:09:47 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,24 +2,24 @@
 run_analysis <- function() {
 
 ## read subject training and test data into R, and merge them to a new data set named "subject"
-subject_train <- read.table("UCI_HAR_Dataset/train/subject_train.txt")    
-subject_test <- read.table("UCI_HAR_Dataset/test/subject_test.txt")       
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")    
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")       
 subject <- rbind(subject_train, subject_test)                             
 names(subject) <- "subject"                                               
 
 ## read y training and test data into R, and merge them to a new data set named "activity"
-y_train <- read.table("UCI_HAR_Dataset/train/y_train.txt")                
-y_test <- read.table("UCI_HAR_Dataset/test/y_test.txt")                   
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")                
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")                   
 y <- rbind(y_train, y_test)                                                
 names(y) <- "activity"                                                    
 
 ## read x training and test data into R, and merge them to a new data set named "x"
-x_train <- read.table("UCI_HAR_Dataset/train/X_train.txt")                 
-x_test <- read.table("UCI_HAR_Dataset/test/X_test.txt")                   
+x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")                 
+x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")                   
 x <- rbind(x_train, x_test)                                               
 
 ## read features.txt and assign the second column content to the names of x data
-features <- read.table("UCI_HAR_Dataset/features.txt") 
+features <- read.table("./UCI HAR Dataset/features.txt") 
 names(x) <- features[, 2] 
 
 ## merge the training and the test sets to create one data set
